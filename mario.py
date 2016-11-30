940d4115ae1485500600c0d2401ed4777af43b82
Yehor0110 <mcshka111@gmail.com>
2016-11-30 06:22:48 +0200

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,4 +1,4 @@
-def pyrid():
+def pyramid():
     while True:
         height = int(input("Enter height: "))
         if 0 < height < 23:
@@ -9,4 +9,4 @@ def pyrid():
                     print("#", sep=' ', end='')
                 print()
             break
-pyrid()
+pyramid()
